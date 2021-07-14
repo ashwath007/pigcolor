b3bfd366a293fa8809906c32b3d3115504dec823
vicky <srivicky2000@gmail.com>
2021-07-14 13:39:52 +0530

    First testing

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 #!/usr/bin/env node
 
-const { BgRed } = require("./assets/colors");
+const express = require('express');
+// const { BgRed } = require("./assets/colors");
+console.log('\x1b[36m%s\x1b[0m', "Welcome to pigcolor");
 
-export const color = (obj) => {
+exports.Color = (obj) => {
+    // console.log(`\x1b[36m%s\x1b[0m`, obj);
     console.log('\x1b[36m%s\x1b[0m', obj);
-    // console.log('\x1b[36m%s\x1b[0m', 'I am cyan'); 
 }
